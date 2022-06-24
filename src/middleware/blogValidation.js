df88445dd95b86c3080fca137bad7619d6013340
lavverma <lavverma109@gmail.com>
2022-06-24 22:47:23 +0530

    final

--- src/middleware/blogValidation.js
+++ src/middleware/blogValidation.js
@@ -2,89 +2,81 @@ const authorModel = require("../models/authorModel");
 const blogModel = require("../models/blogModel");
 // var validator = require('validator')
 
-let bolgSchemaValidation=async function(req,res,next){
-    let data=req.body
-    if(data.title){
-        if(typeof (data.title)=== "string"){
-            if(data.body){
-                if(typeof (data.body)==="string"){
-                    if(data.authorId){
-                        let check=await authorModel.findById(data.authorId)
-                        if(check!=null){
-                            if(typeof(data.tags)==="string" || !(data.tags)){
-                                if(data.category){
-                                    if(typeof (data.category)==="string"){
-                                        if(typeof(data.subcategory)==="string" || !(data.subcategory)){
-                                            if((/^(true|false|True|False|TRUE|FALSE)$/).test(data.isPublished) && typeof(data.isPublished) != "string"
-                                            ){
-                                            next()
-                                        }else{
-                                            res.status(400).send({error:"please give publisher true or false" })
+let bolgSchemaValidation = async function (req, res, next) {
+    try {
+        let data = req.body
+        if (data.title) {
+            if (typeof (data.title) === "string") {
+                if (data.body) {
+                    if (typeof (data.body) === "string") {
+                        if (data.authorId) {
+                            let check = await authorModel.findById(data.authorId)
+                            if (check != null) {
+                                if (typeof (data.tags) === "string" || !(data.tags)) {
+                                    if (data.category) {
+                                        if (typeof (data.category) === "string") {
+                                            if (typeof (data.subcategory) === "string" || !(data.subcategory)) {
+                                                if ((/^(true|false|True|False|TRUE|FALSE)$/).test(data.isPublished) && typeof (data.isPublished) != "string"
+                                                ) {
+                                                    next()
+                                                } else {
+                                                    res.status(400).send({ error: "please give publisher true or false" })
+                                                }
+                                            } else {
+                                                res.status(400).send({ error: "please give subcategory in string" })
+                                            }
+                                        } else {
+                                            res.status(400).send({ error: "please give category in string" })
                                         }
-                                        }else{
-                                            res.status(400).send({error:"please give subcategory in string" })
-                                        }
-                                    }else{
-                                        res.status(400).send({error:"please give category in string"})
+                                    } else {
+                                        res.status(400).send({ error: "category is mandatory" })
                                     }
-                                }else{
-                                    res.status(400).send({error:"category is mandatory"})
+                                } else {
+                                    res.status(400).send({ error: "please give tags in string" })
                                 }
-                            }else{
-                                res.status(400).send({error: "please give tags in string" })
+                            } else {
+                                res.status(400).send({ error: "author not valid" })
                             }
-                        }else{
-                            res.status(400).send({error:"author not valid"})
+                        } else {
+                            res.status(400).send({ error: "authorId is mandatory" })
                         }
-                    }else{
-                        res.status(400).send({error: "authorId is mandatory"})
+                    } else {
+                        res.status(400).send({ error: "please give body in string" })
                     }
-                }else{
-                    res.status(400).send({error:"please give body in string"})
+                } else {
+                    res.status(400).send({ error: "body is mandatory" })
                 }
-            }else{
-                res.status(400).send({error: "body is mandatory"})
+            } else {
+                res.status(400).send({ error: "please give title in string" })
             }
-        }else{
-            res.status(400).send({error:"please give title in string"})
+        } else {
+            res.status(400).send({ error: "title is mandatory" })
         }
-    }else{
-        res.status(400).send({error:"title is mandatory"})
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
     }
 }
-module.exports.bolgSchemaValidation=bolgSchemaValidation
+
+module.exports.bolgSchemaValidation = bolgSchemaValidation
+
 //////////////////////////////////////////////////////////////////////
 
-let blogIdValidate=async function(req,res,next){
-    try{
-let blogId=req.params.blogId
-let check=await blogModel.findById(blogId)
-    if(!check){
-        return res.status(404).send({status:false, msg: ""})
+let blogIdValidate = async function (req, res, next) {
+    try {
+        let blogId = req.params.blogId
+        let check = await blogModel.findById(blogId)
+        if (!check) {
+            return res.status(404).send({ status: false, msg: "" })
+        }
+
+        next()
     }
-    
-    next()
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
 }
-catch(err){
-  res.status(500).send({error: err.message})
-}
-}
- module.exports.blogIdValidate=blogIdValidate
+module.exports.blogIdValidate = blogIdValidate
 
 
 //////////////////////////////////////////////////////////////////////////////////
-
-// let checkValidate=async function(req,res,next){
-//     try{
-// let blogId=req.query._id
-// let check=await blogModel.findById(blogId)
-//     if(!check){
-//         res.status(404).send({status:false, msg: ""})
-//     }
-//     next()
-// }
-// catch(err){
-//     res.status(500).send({error: err.message})
-// }
-// }
-// module.exports.checkValidate=checkValidate
